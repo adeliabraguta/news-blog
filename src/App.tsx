2a35fcc9ca7dd9ae299e187bb1af79fb398639a6
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 14:03:29 +0300

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes/routes.tsx";
 
+const LoadingSpinner = lazy(() => import("./components/UI/LoadingSpinner.tsx"));
+
 function App() {
 	const routing = useRoutes(routes);
-	return <Suspense fallback={<div>Loading...</div>}>{routing}</Suspense>;
+	return <Suspense fallback={<LoadingSpinner />}>{routing}</Suspense>;
 }
 
 export default App;
