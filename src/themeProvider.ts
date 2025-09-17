d65206ab894868095dca8a442248e6d84a8b6784
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 23:42:34 +0300

    fix theme provider

--- src/themeProvider.ts
+++ src/themeProvider.ts
@@ -1,21 +1,17 @@
-import { createTheme } from "@mui/material/styles";
+import {createTheme} from "@mui/material/styles";
 
 const theme = createTheme({
-	colorSchemes: {
-		light: {
-			palette: {
-				primary: {
-					main: "#1976d2",
-					light: "#42a5f5",
-					dark: "#1565c0",
-					contrastText: "#fff",
-				},
-				secondary: {
-					main: "#dc004e",
-				},
-			},
-		},
-	},
+    palette: {
+        primary: {
+            500: "#1976d2",
+            600: "#42a5f5",
+            700: "#1565c0",
+            contrastText: "#fff",
+        },
+        secondary: {
+            500: "#dc004e",
+        },
+    },
 });
 
 export default theme;
