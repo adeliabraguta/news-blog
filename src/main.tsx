b274b3a9efb66e35b9faa5f51e54459e1870b876
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 15:00:59 +0300

    Fix gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,9 @@ const theme = createTheme({
 	palette: {
 		primary: {
 			main: "#1976d2",
+			light: "#42a5f5",
+			dark: "#1565c0",
+			contrastText: "#fff",
 		},
 		secondary: {
 			main: "#dc004e",
