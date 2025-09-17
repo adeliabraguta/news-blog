7b189aeba4b87cb0bf872e3c32987275932e7d76
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 13:04:21 +0300

    Add biome formating

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,25 +1,29 @@
-import {useGlobalStore} from "../store/globalStore.ts";
-import {Button} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useGlobalStore } from "../store/globalStore.ts";
 
 const HeaderComponent = () => {
-    const navigate = useNavigate()
-    const {user, setUser} = useGlobalStore()
+	const navigate = useNavigate();
+	const { user, setUser } = useGlobalStore();
 
-    const onLogout = () => {
-        setUser('')
-        navigate("/auth")
-    }
+	const onLogout = () => {
+		setUser("");
+		navigate("/auth");
+	};
 
-    return (
-        <div className={"p-3 border-b border-sky-500 flex justify-between items-center gap-5"}>
-            <h2 className={"text-xl text-sky-900 italic"}>Hews Today</h2>
-            <div className={"flex justify-between items-center gap-4"}>
-                <p>Welcome, <span className={"text-sky-700"}>{user}</span>!</p>
-                <Button onClick={() => onLogout()} type={"button"} variant="outlined">Log Out</Button>
-            </div>
-        </div>
-    );
+	return (
+		<div className={"p-3 border-b border-sky-500 flex justify-between items-center gap-5"}>
+			<h2 className={"text-xl text-sky-900 italic"}>Hews Today</h2>
+			<div className={"flex justify-between items-center gap-4"}>
+				<p>
+					Welcome, <span className={"text-sky-700"}>{user}</span>!
+				</p>
+				<Button onClick={() => onLogout()} type={"button"} variant="outlined">
+					Log Out
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default HeaderComponent;
--- src/layouts/MainProtectedLayout.tsx
+++ src/layouts/MainProtectedLayout.tsx
@@ -1,26 +1,26 @@
-import {Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import FooterComponent from "../components/FooterComponent";
 import HeaderComponent from "../components/HeaderComponent";
-import {useGlobalStore} from "../store/globalStore.ts";
+import { useGlobalStore } from "../store/globalStore.ts";
 
 const MainProtectedLayout = () => {
-    const {user} = useGlobalStore();
+	const { user } = useGlobalStore();
 
-    if (!user) {
-        return <Navigate to="/auth"/>;
-    }
+	if (!user) {
+		return <Navigate to="/auth" />;
+	}
 
-    return (
-        <div className={"bg-neutral-200 h-dvh"}>
-            <div className="h-dvh bg-amber-50 w-full max-w-6xl px-4 mx-auto">
-                <HeaderComponent/>
-                <main className="py-8">
-                    <Outlet/>
-                </main>
-                <FooterComponent/>
-            </div>
-        </div>
-    );
+	return (
+		<div className={"bg-neutral-200 h-dvh"}>
+			<div className="h-dvh bg-amber-50 w-full max-w-6xl px-4 mx-auto">
+				<HeaderComponent />
+				<main className="py-8">
+					<Outlet />
+				</main>
+				<FooterComponent />
+			</div>
+		</div>
+	);
 };
 
 export default MainProtectedLayout;
