fa5d0e7cb62b1a2832b79ea2df18cc1bdd5a89cc
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 13:03:04 +0300

    Add header stylization and functionality

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,9 +1,25 @@
+import {useGlobalStore} from "../store/globalStore.ts";
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
 const HeaderComponent = () => {
-	return (
-		<div>
-			<p>Header</p>
-		</div>
-	);
+    const navigate = useNavigate()
+    const {user, setUser} = useGlobalStore()
+
+    const onLogout = () => {
+        setUser('')
+        navigate("/auth")
+    }
+
+    return (
+        <div className={"p-3 border-b border-sky-500 flex justify-between items-center gap-5"}>
+            <h2 className={"text-xl text-sky-900 italic"}>Hews Today</h2>
+            <div className={"flex justify-between items-center gap-4"}>
+                <p>Welcome, <span className={"text-sky-700"}>{user}</span>!</p>
+                <Button onClick={() => onLogout()} type={"button"} variant="outlined">Log Out</Button>
+            </div>
+        </div>
+    );
 };
 
 export default HeaderComponent;
