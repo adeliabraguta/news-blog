2a9ad4ab9325ed533703b82b849e2071e4be3c99
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 23:36:29 +0300

    fix theme provider

--- src/components/UI/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress } from "@mui/material";
 const LoadingSpinner = () => {
 	return (
 		<Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-			<CircularProgress color="primary" />
+			<CircularProgress />
 		</Box>
 	);
 };
