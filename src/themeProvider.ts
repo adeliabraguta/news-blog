eaf569e2f41c03bd23937a8e212a625c6fc9aa30
adeliabraguta <adeliabraguta@gmail.com>
2025-09-17 23:43:50 +0300

    fix linting

--- src/themeProvider.ts
+++ src/themeProvider.ts
@@ -1,17 +1,17 @@
-import {createTheme} from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            500: "#1976d2",
-            600: "#42a5f5",
-            700: "#1565c0",
-            contrastText: "#fff",
-        },
-        secondary: {
-            500: "#dc004e",
-        },
-    },
+	palette: {
+		primary: {
+			500: "#1976d2",
+			600: "#42a5f5",
+			700: "#1565c0",
+			contrastText: "#fff",
+		},
+		secondary: {
+			500: "#dc004e",
+		},
+	},
 });
 
 export default theme;
